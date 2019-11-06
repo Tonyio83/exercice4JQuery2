1fe0aab599ac0188d09746421617c44aac0aed78
Tonyio83 <anthonyfrancelle@gmail.com>
2019-11-06 09:13:14 +0100

    exercice 4 correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
-$('.color').click(function(){//crée une fonction sur le clic d'une couleur
-  $(this).each(function(){//crée une fonction sur toutes les couleurs
-    var myColor = $(this).attr('id')//récupère la valeur sur l'id dans une variable
-    $('#text').css('color', myColor)//change la couleur en fonction de la couleur selectionné
+$(function(){
+  $('.color').click(function(){//appel de la fonction sur le clic d'une couleur
+    $(this).each(function(){//appel de la fonction sur toutes les couleurs
+      var myColor = $(this).attr('id')//récupère la valeur sur l'id dans une variable
+      $('#text').css('color', myColor)//change la couleur en fonction de la couleur selectionné
+    })
   })
 })
